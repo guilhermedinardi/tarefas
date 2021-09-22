88973898968af6ab6c39fc2f68f86ef3500d3579
Guilherme Dinardi <gdinardito@gmail.com>
2021-09-21 23:24:08 -0300

    fix removed console.log

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Task from './Task'
 
 const Tasks = ({ tasks, handleTaskCompleted, handleTaskDeleted }) => {
-    console.log(tasks)
     return(
         <>
             {tasks.map(task => ( 
